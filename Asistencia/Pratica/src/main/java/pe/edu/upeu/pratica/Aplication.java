199df14716d74e2d9b41413a250a2361a45ea581
juan <juanbenique170@gmail.com>
2025-08-21 10:45:59 -0500

    sdsd

---
+++ Asistencia/Pratica/src/main/java/pe/edu/upeu/pratica/Aplication.java
@@ -0,0 +1,12 @@
+package pe.edu.upeu.pratica;
+
+import javafx.stage.Stage;
+
+public abstract class Aplication{
+   public static void main(String[] args) {
+       System.out.println("Iniciando application DMP");
+       PraticaApplication.main(args);
+   }
+
+
+}
---
+++ Asistencia/Pratica/src/main/java/pe/edu/upeu/pratica/contorl/AsistenciaController.java
@@ -0,0 +1,7 @@
+package pe.edu.upeu.pratica.contorl;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AsistenciaController {
+}
